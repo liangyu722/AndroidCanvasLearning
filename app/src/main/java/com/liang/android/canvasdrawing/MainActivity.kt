de3111c1465175f6fb2be80e5c23a8a3a3165e57
liangyu <liangyu722@gmail.com>
2020-05-14 23:13:25 +1000

    Init commit

---
+++ app/src/main/java/com/liang/android/canvasdrawing/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.liang.android.canvasdrawing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+    }
+}
